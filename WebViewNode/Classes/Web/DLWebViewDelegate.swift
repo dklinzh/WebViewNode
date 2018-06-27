7d4d10f790ba8c5386e525153f6a6da5dc3aa50d
Daniel Lin <linzhdk@gmail.com>
2018-06-27 11:11:26 +0800

    Rename the methods of protocol DLWebViewDelegate & add comments for the functions

--- WebViewNode/Classes/Web/DLWebViewDelegate.swift
+++ WebViewNode/Classes/Web/DLWebViewDelegate.swift
@@ -8,17 +8,50 @@
 import WebKit
 
 public protocol DLWebViewDelegate: class {
-    func dl_webView(_ webView: DLWebView, didStartLoading url: URL?)
-    func dl_webView(_ webView: DLWebView, didFinishLoading url: URL?)
-    func dl_webView(_ webView: DLWebView, didFailToLoad url: URL?, error: Error?)
-    func dl_webView(_ webView: DLWebView, decidePolicyFor navigationResponse: WKNavigationResponse) -> WKNavigationResponsePolicy
-    func dl_webView(_ webView: DLWebView, decidePolicyFor navigationAction: WKNavigationAction) -> WKNavigationActionPolicy
+    
+    /// Invoked when URL request of the main frame navigation starts loading.
+    ///
+    /// - Parameters:
+    ///   - webView: The web view invoking the delegate method.
+    ///   - url: The active URL of web view
+    func webView(_ webView: DLWebView, didStartLoading url: URL?)
+    
+    /// Invoked when URL request of the main frame navigation completes.
+    ///
+    /// - Parameters:
+    ///   - webView: The web view invoking the delegate method.
+    ///   - url: The active URL of web view
+    func webView(_ webView: DLWebView, didFinishLoading url: URL?)
+    
+    /// Invoked when an error occurs while loading data for the main frame.
+    ///
+    /// - Parameters:
+    ///   - webView: The web view invoking the delegate method.
+    ///   - url: The active URL of web view
+    ///   - error: The error that occurred.
+    func webView(_ webView: DLWebView, didFailToLoad url: URL?, error: Error?)
+    
+    /// Decides whether to allow or cancel a navigation while starting to load.
+    ///
+    /// - Parameters:
+    ///   - webView: The web view invoking the delegate method.
+    ///   - navigationAction: Descriptive information about the action triggering the navigation request.
+    /// - Returns: The policy to pass back to the decision handler from the webView(_:decidePolicyFor:decisionHandler:) method.
+    func webView(_ webView: DLWebView, decidePolicyFor navigationAction: WKNavigationAction) -> WKNavigationActionPolicy
+    
+    /// Decides whether to allow or cancel a navigation after its response is known.
+    ///
+    /// - Parameters:
+    ///   - webView: The web view invoking the delegate method.
+    ///   - navigationResponse: Descriptive information about the navigation response.
+    /// - Returns: The policy to pass back to the decision handler from the webView(_:decidePolicyFor:decisionHandler:) method.
+    func webView(_ webView: DLWebView, decidePolicyFor navigationResponse: WKNavigationResponse) -> WKNavigationResponsePolicy
 }
 
 public extension DLWebViewDelegate {
-    func dl_webView(_ webView: DLWebView, didStartLoading url: URL?) {}
-    func dl_webView(_ webView: DLWebView, didFinishLoading url: URL?) {}
-    func dl_webView(_ webView: DLWebView, didFailToLoad url: URL?, error: Error?) {}
-    func dl_webView(_ webView: DLWebView, decidePolicyFor navigationResponse: WKNavigationResponse) -> WKNavigationResponsePolicy { return .allow }
-    func dl_webView(_ webView: DLWebView, decidePolicyFor navigationAction: WKNavigationAction) -> WKNavigationActionPolicy { return .allow }
+    func webView(_ webView: DLWebView, didStartLoading url: URL?) {}
+    func webView(_ webView: DLWebView, didFinishLoading url: URL?) {}
+    func webView(_ webView: DLWebView, didFailToLoad url: URL?, error: Error?) {}
+    func webView(_ webView: DLWebView, decidePolicyFor navigationAction: WKNavigationAction) -> WKNavigationActionPolicy { return .allow }
+    func webView(_ webView: DLWebView, decidePolicyFor navigationResponse: WKNavigationResponse) -> WKNavigationResponsePolicy { return .allow }
 }
